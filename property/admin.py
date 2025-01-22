cf27e99a7b90e591304128270986e199a1061042
Avakova Anastasiia <avakova.ast@gmail.com>
2025-01-22 17:05:33 +0300

    Add OwnerInline to display and edit owners on Flat page

--- property/admin.py
+++ property/admin.py
@@ -3,23 +3,6 @@ from .models import Flat
 from .models import Complaint, Owner
 
 
-class FlatAdmin(admin.ModelAdmin):
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone']
-    list_editable = ['new_building']
-
-    readonly_fields = ['created_at']
-    readonly_fields = ['owner_pure_phone']
-
-    search_fields = ['town', 'address', 'owner']
-
-    list_filter = ['new_building', 'rooms_number', 'construction_year', 'town', 'has_balcony']
-
-    raw_id_fields = ['liked_by']
-    raw_id_fields = ['owners']
-
-
-admin.site.register(Flat, FlatAdmin)
-
 
 class ComplaintAdmin(admin.ModelAdmin):
     list_display = ['user', 'flat', 'created_at']
@@ -39,3 +22,27 @@ class OwnerAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Owner, OwnerAdmin)
+
+
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ['owner', 'flat']
+    extra = 1
+
+
+class FlatAdmin(admin.ModelAdmin):
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone']
+    list_editable = ['new_building']
+
+    readonly_fields = ['created_at']
+    readonly_fields = ['owner_pure_phone']
+
+    search_fields = ['town', 'address', 'owner']
+
+    list_filter = ['new_building', 'rooms_number', 'construction_year', 'town', 'has_balcony']
+
+    raw_id_fields = ['liked_by']
+    raw_id_fields = ['owners']
+    inlines = [OwnerInline]
+
+admin.site.register(Flat, FlatAdmin)
